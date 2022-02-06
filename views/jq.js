b98dcc7a241f884c790052e4a3a4be11a81bb13e
T1ktaalik <88627980+T1ktaalik@users.noreply.github.com>
2022-02-06 11:25:23 +0300

    06022022v1

--- views/jq.js
+++ views/jq.js
@@ -1,4 +1,12 @@
 $(document).ready(() => { 
-   $.get("http://localhost:3000/buckets", (data) =>{console.log(data.body.items); $('#list').text(data.body.items);})
-   
+   $.get("http://localhost:3000/buckets", (data) =>{
+      $('#quantity').text(`Всего файлов ${data.body.items.length}`)
+      $('#list').text('Файлы  проекта:')
+      let myObjects = data.body.items;
+      for (let i = 0; i < myObjects.length; i++) {
+      console.log(myObjects[i].bucketKey);
+      $('#list').append(`<br> ${myObjects[i].bucketKey}`)      
+   }
 })
+})
+  
