25bdc380d664e58a1e335d81ffa7cff2d8833857
Sergey <88627980+T1ktaalik@users.noreply.github.com>
2022-02-07 23:28:44 +0300

    Merge branch 'main' of https://github.com/NovaEngineering/elma2d3d

--- start.js
+++ start.js
@@ -15,11 +15,8 @@ function getClient(scopes) { return new ForgeSDK.AuthClientTwoLegged(client_id, 
 /*
 * Маршрутизация
 */
-<<<<<<< HEAD
+
 //Главная страница ++++++++++...Это роутинг
-=======
-//Главная страница 
->>>>>>> b6ab6c68b797b118ee7a7b3b01307f8aa9660cda
 app.get('/', function(req, res) {
         res.render('index')
 });
@@ -43,26 +40,16 @@ app.get('/bucket_details', async (req, res, next) => {
     try{
     tokenInternal = await getClient(scopesInternal).authenticate();
     let BucketsApi = new ForgeSDK.BucketsApi();
-<<<<<<< HEAD
+
     let bucketDetails = await BucketsApi.getBucketDetails("warestore2", getClient(scopesInternal), tokenInternal);
     res.send(bucketDetails);
     console.log(bucketDetails);
 }catch (err) {next(err)}
-    //"warestore2"
-    //getBucketDetails(bucketKey, oauth2client, credentials)
 })
 //Шаблонизация ...Это middleware
 app.set('views', path.join(__dirname,'./views') ); 
 app.set('view engine', 'pug')
 //Слушаем порт.... Это middleware
-=======
-    sampleBucketDetails = await BucketsApi.getBucketDetails("warestore2", getClient(scopesInternal), tokenInternal);
-    res.send(sampleBucketDetails)
-} catch (err) {next(err)}
-})
-
-// Пробуем buckets/:bucketKey/objects/:objectName/details 
-
 // Пробуем получить перечень форматов getFormats(opts, oauth2client, credentials)
 app.get('/formats', async (req, res, next) => {
     try{
@@ -73,10 +60,7 @@ app.get('/formats', async (req, res, next) => {
    
 } catch (err) {next(err)}
 })
-
 // 	buckets/:bucketKey/objects пробуем получить urn = bucket ID
-// line 227 of readme.md forge-apus*ForgeSdk.ObjectsApi* | [**getObjects**](docs/ObjectsApi.md#getObjects) | **GET** /oss/v2/buckets/{bucketKey}/objects |
-// line 201 of Objects API forge-apis getObjects(bucketKey, opts, oauth2client, credentials)
 app.get('/bucketId', async (req, res, next) => {
     try{
     tokenInternal = await getClient(scopesInternal).authenticate();
@@ -86,17 +70,15 @@ app.get('/bucketId', async (req, res, next) => {
     
 } catch (err) {next(err)}
 })
-
-
-
 ////Middleware
 //Шаблонизация
 app.set('views', path.join(__dirname,'./views') );
 app.set('view engine', 'pug')
-//
-
 //Слушаем порт.
->>>>>>> b6ab6c68b797b118ee7a7b3b01307f8aa9660cda
 app.listen(port, console.log(`Приложение ${appName}" работает на порту "${port}"`));
 
-
+//Error handling - не пойму, зачем
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Это не работает')
+})
