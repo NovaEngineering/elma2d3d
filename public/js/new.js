c6b04fdb3f2ebb58acb2b75e882922da76fd3a3e
T1ktaalik <88627980+T1ktaalik@users.noreply.github.com>
2022-02-21 19:41:25 +0300

    21022022v1

--- public/js/new.js
+++ public/js/new.js
@@ -1,24 +1,63 @@
-
-
-/*
-var vm = new Vue ({
-    el: '#vuetree',
-data: {
-    message: x
-}});
-*/
-var vm = new Vue ({
-    el: "#vuetree",
-    data() {
-        return {
-            info: null, //
-        }
-    },
-    mounted() {
-        axios
-            .get('http://localhost:3000/buckets_raw')
-            .then(response => (this.info = response))
+   
+   var treeData = {
+        name: "Перечень файлов",
+        children: [
+            //{name: "warestore2"},
+            //{name: "warestore3"}
+        ]
+  
     }
-})
+    
 
+ 
+ //x.textContent = 'from there to here'
+  Vue.component("tree-item", {
+    template: "#item-template",
+    props: {
+      item: Object
+    },
+    data: function() {
+      return {
+        isOpen: false
+      };
+    },
+    computed: {
+      isFolder: function() {
+        return this.item.children && this.item.children.length;
+      }
+    },
+    methods: {
+      toggle: function() {
+        if (this.isFolder) {
+          this.isOpen = !this.isOpen;
+        }
+      },
+      makeFolder: function() {
+        if (!this.isFolder) {
+          this.$emit("make-folder", this.item);
+          this.isOpen = true;
+        }
+      }
+    }
+  });
 
+  // boot up the demo
+  var demo = new Vue({
+    el: "#demo",
+    data: {
+      treeData: treeData
+    },
+    methods: {
+      makeFolder: function(item) {
+        Vue.set(item, "children", []);
+        this.addItem(item);
+      },
+      addItem: function(item) {
+        item.children.push({
+          name: "new stuff"
+        });
+      }
+    }
+  });
+
+  console.log(document.styleSheets)
